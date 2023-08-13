c180be0b51edf190129d68d91440a9c59afe8b4d
Reed0610 <shanereed19@gmail.com>
2023-08-13 20:36:29 +0000

    commit 2
    Signed-off-by: Reed0610 <shanereed19@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,34 +1,47 @@
 #include "mbed.h"
 #include "LSM6DSLSensor.h"
-#include "RGBLed.h"
 
-I2C i2c(I2C_SDA, I2C_SCL);  // Create I2C communication instance
-LSM6DSLSensor acc_gyro(&i2c);  // Instantiate the LSM6DSLSensor object using I2C
-RGBLed led(LED1_R, LED1_G, LED1_B);  // Instantiate the RGBLed object
+// Define LED objects
+PwmOut redLED(LED1);
+PwmOut greenLED(LED2);
+PwmOut blueLED(LED3);
+
+// Create an instance of DevI2C and LSM6DSLSensor
+static DevI2C devI2c(PB_11, PB_10);
+static LSM6DSLSensor acc_gyro(&devI2c, 0xD4, D4, D5); // high address
+
+// Function to map input value to output range
+float mapValue(float value, float inMin, float inMax, float outMin, float outMax) {
+    return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
 
 int main() {
-    // Initialize the accelerometer
-    acc_gyro.init(nullptr);
+    uint8_t id;
+    int32_t axes[3];
+
+    // Initialize accelerometer and gyroscope
+    acc_gyro.init(NULL);
     acc_gyro.enable_x();
-    acc_gyro.enable_y();
-    acc_gyro.enable_z();
 
     while (1) {
-        MotionSensorData_axes_t axes_data;
-        acc_gyro.GetAxes(&axes_data);
+        // Read accelerometer axes
+        acc_gyro.get_x_axes(axes);
 
-        int16_t x_acc = axes_data.x;
-        int16_t y_acc = axes_data.y;
-        int16_t z_acc = axes_data.z;
+        // Map accelerometer data to LED brightness range
+        float redBrightness = mapValue(static_cast<float>(axes[0]), -2000.0, 2000.0, 0.01, 0.99);
+        float greenBrightness = mapValue(static_cast<float>(axes[1]), -2000.0, 2000.0, 0.01, 0.99);
+        float blueBrightness = mapValue(static_cast<float>(axes[2]), -2000.0, 2000.0, 0.01, 0.99);
 
-        // Normalize accelerometer data to RGB color range (0-255)
-        uint8_t red_brightness = static_cast<uint8_t>((x_acc + 32768) / 128);
-        uint8_t green_brightness = static_cast<uint8_t>((y_acc + 32768) / 128);
-        uint8_t blue_brightness = static_cast<uint8_t>((z_acc + 32768) / 128);
+        // Print accelerometer data and calculated brightness values
+        printf("Acc X: %d, Acc Y: %d, Acc Z: %d\r\n", axes[0], axes[1], axes[2]);
+        printf("Red Brightness: %f, Green Brightness: %f, Blue Brightness: %f\r\n", redBrightness, greenBrightness, blueBrightness);
 
-        // Set the LED color based on accelerometer data
-        led.setColour(red_brightness, green_brightness, blue_brightness);
+        // Set LED brightness levels
+        redLED = redBrightness;
+        greenLED = greenBrightness;
+        blueLED = blueBrightness;
 
-        ThisThread::sleep_for(std::chrono::milliseconds(100)); // Delay for a while
+        // Wait for a short duration
+        ThisThread::sleep_for(2000ms);
     }
 }
